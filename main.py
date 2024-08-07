4300f4ba9c4e098eb5f1f56220974e8070f0fc1a
Heyson <heysonsilva98@gmail.com>
2024-08-07 00:48:32 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,21 @@
+# Tendo por base o arquivo de logs de um servidor, o programa que responde às seguintes questões:
+# Quantos IPs distintos se conectaram ao servidor?
+# Qual o IP que mais vezes conectou ao servidor?
+# Em que minuto houve mais conexões ao servidor?
+
+# arquivo = open('apache_logs.txt', 'r')
+arquivo = open('teste.txt', 'r')
+lines = arquivo.readline().split()
+
+ipList = []
+
+print (lines[0])
+   
+
+
+
+
+
+
+
+
